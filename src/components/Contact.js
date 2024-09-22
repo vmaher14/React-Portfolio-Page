7610fe77365131ea07d97bdb3b8a2e43b4544610
Victor Maher <vmaher14@yahoo.com>
2024-09-22 16:56:15 -0700

    Updated fetch link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,7 +25,7 @@ export const Contact = () => {
     const handleSubmit = async (e) => {
       e.preventDefault();
       setButtonText("Sending...");
-      let response = await fetch("http://localhost:5000/contact", {
+      let response = await fetch("https://victormaherwebportfolio.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
